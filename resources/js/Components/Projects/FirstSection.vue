e6c5b81fc07b6ecaa5176cc81be477abbfefe9e3
mthiagoalves <m.thiago.alves@live.com>
2024-03-21 18:40:48 +0000

    chore(projects): order cards

--- resources/js/Components/Projects/FirstSection.vue
+++ resources/js/Components/Projects/FirstSection.vue
@@ -10,17 +10,29 @@
             />
         </div>
         <div class="flex justify-center flex-col order-1 sm:order-2">
-            <div class="m-24 h-full border border-gray-950 rounded-lg"></div>
+            <div
+                class="m-24 h-full border border-gray-950 rounded-lg flex items-center justify-center flex-col"
+            >
+                <h3 class="titles-aurelie">Campo Pequeno</h3>
+                <p class="sub-title-card-projects">Apartment</p>
+                <span class="years-project mb-6">2023</span>
+            </div>
         </div>
-        <div class="order-3 sm:order-4">
+        <div class="order-4 sm:order-4">
             <img
                 class="h-auto max-w-full ml-auto"
                 src="https://dummyimage.com/1100x1000/e9b8b1/fff"
                 alt=""
             />
         </div>
-        <div class="flex justify-center flex-col order-4 sm:order-3">
-            <div class="m-24 h-full border border-gray-950 rounded-lg"></div>
+        <div class="flex justify-center flex-col order-3 sm:order-3">
+            <div
+                class="m-24 h-full border border-gray-950 rounded-lg flex items-center justify-center flex-col"
+            >
+                <h3 class="titles-aurelie">Braganca</h3>
+                <p class="sub-title-card-projects">Apartment</p>
+                <span class="years-project mb-6">2022</span>
+            </div>
         </div>
         <div class="order-6 sm:order-5">
             <img
@@ -30,19 +42,48 @@
             />
         </div>
         <div class="flex justify-center flex-col order-5 sm:order-6">
-            <div class="m-24 h-full border border-gray-950 rounded-lg"></div>
+            <div
+                class="m-24 h-full border border-gray-950 rounded-lg flex items-center justify-center flex-col"
+            >
+                <h3 class="titles-aurelie">Parque das Nacoes</h3>
+                <p class="sub-title-card-projects">Apartment</p>
+                <span class="years-project mb-6">2022</span>
+            </div>
         </div>
-        <div class="order-7 sm:order-8">
+        <div class="order-8 sm:order-8">
             <img
                 class="h-auto max-w-full ml-auto"
                 src="https://dummyimage.com/1100x1000/e9b8b1/fff"
                 alt=""
             />
         </div>
-        <div class="flex justify-center flex-col order-8 sm:order-7">
-            <div class="m-24 h-full border border-gray-950 rounded-lg"></div>
+        <div class="flex justify-center flex-col order-7 sm:order-7">
+            <div
+                class="m-24 h-full border border-gray-950 rounded-lg flex items-center justify-center flex-col"
+            >
+                <h3 class="titles-aurelie">Holiday</h3>
+                <p class="sub-title-card-projects">Apartment</p>
+                <span class="years-project mb-6">2021</span>
+            </div>
         </div>
-
-
     </div>
 </template>
+
+<style scoped>
+.sub-title-card-projects {
+    color: #000;
+    text-align: center;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.years-project {
+    color: #808080;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+</style>
