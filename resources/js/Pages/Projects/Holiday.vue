24dd987d386cb1e92ab8ced52445f8eba5e15ef4
mthiagoalves <m.thiago.alves@live.com>
2024-03-22 10:22:38 +0000

    feat(Projects: create individual page of the projects

---
+++ resources/js/Pages/Projects/Holiday.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+import FirstSection from "@/Components/Projects/FirstSection.vue";
+</script>
+
+<template>
+    <Head>
+        <title>Projects - Ana Paiva Decor</title>
+    </Head>
+
+    <MainLayout>
+        <template #main>
+            <FirstSection> </FirstSection>
+        </template>
+    </MainLayout>
+</template>
